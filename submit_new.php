c97be713089455c9027e7ea73fffd7483eb862c7
Natalie Gosnell <gosnell@astro.as.utexas.edu>
2016-06-11 17:00:21 +0200

    Change background color, button colors

---
+++ submit_new.php
@@ -0,0 +1,15 @@
+<?php
+  // Small script to save the values to a single CSV file
+  $date = date_create();
+  $log_file_name = "data.csv"; // Change to the log file name
+  $talkref = $_POST['sessionID'];
+  $chair_gender = $_POST['chairGender'];
+  $speaker_gender = $_POST['speakerGender'];
+  $questions = $_POST['questions'];
+  $openform = $_POST['OpenComments'];
+  $epoch = date_format($date, 'U');
+  $timedate = date_format($date, 'Y-m-d H:i:s');
+  $message = "{$epoch},{$timedate},{$talkref},{$chair_gender},{$speaker_gender},{$questions},{$openform} ;\n";
+  file_put_contents($log_file_name, $message, FILE_APPEND);
+  header('Location: ./form.html'); // redirect back to the main site
+?>
